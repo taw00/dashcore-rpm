90076cac27de560412dc7469bc608a9065de2077
taw00 <t0dd+gh@protonmail.com>
2017-02-06 13:11:31 +0100

    Update masternode-count.sh

--- utilities/masternode-count.sh
+++ utilities/masternode-count.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
+# If systemd-managed...
 LF="sudo -u dashcore dash-cli -conf=/etc/dashcore/dash.conf masternode list full"
+# If "normal user" deployed..
+#LF="dash-cli masternode list full"
 F=/tmp/tmp-count.txt
 $LF > $F
 echo "Masternodes: $(cat $F | wc -l )"
