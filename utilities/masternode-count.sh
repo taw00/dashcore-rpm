ba226851e9dbf067d3592a687e761a3045c00d4a
Todd Warner <t0dd+gh@protonmail.com>
2017-02-06 07:29:15 -0500

    blah

--- utilities/masternode-count.sh
+++ utilities/masternode-count.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+# run this with 'watch -n15 ./masternode-count.sh'
+
 # If systemd-managed...
 LF="sudo -u dashcore dash-cli -conf=/etc/dashcore/dash.conf masternode list full"
 # If "normal user" deployed..
@@ -8,6 +10,7 @@ LF="sudo -u dashcore dash-cli -conf=/etc/dashcore/dash.conf masternode list full
 F=/tmp/tmp-count.txt
 $LF > $F
 echo "Masternodes: $(cat $F | wc -l )"
+echo "   On 70103: $(cat $F | grep 70103 | wc -l)"
 echo "   On 70206: $(cat $F | grep 70206 | wc -l)"
 echo "    ENABLED: $(cat $F | grep ENABLED | grep -v PRE_ENABLED | wc -l)"
 rm $F
