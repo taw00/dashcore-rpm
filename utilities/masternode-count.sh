d43e604e6b38f5e82a1cf862c585683ebc6c9cc5
Todd Warner <t0dd+gh@protonmail.com>
2017-02-13 10:12:27 -0500

    Beefed up all the percentage stats

--- utilities/masternode-count.sh
+++ utilities/masternode-count.sh
@@ -12,28 +12,48 @@ F=/tmp/tmp-count.txt
 $LF > $F
 
 total=$(cat $F | wc -l )
+
 total_old=$(cat $F | grep 70103 | wc -l)
-percent_old=`python -c "print round($total_old.0 / $total * 100, 2)"`
+percent_old=$(printf '%6.2f' `python -c "print round($total_old.0 / $total * 100, 2)"`)
 
 total_new=$(cat $F | grep 70206 | wc -l)
-percent_new=`python -c "print round($total_new.0 / $total * 100, 2)"`
+percent_new=$(printf '%6.2f' `python -c "print round($total_new.0 / $total * 100, 2)"`)
 
 total_enabled=$(cat $F | grep ENABLED | grep -v PRE_ENABLED | wc -l)
-percent_enabled=`python -c "print round($total_enabled.0 / $total * 100, 2)"`
+percent_enabled=$(printf '%6.2f' `python -c "print round($total_enabled.0 / $total * 100, 2)"`)
+total_not_enabled=$(($total-$total_enabled))
+percent_not_enabled=$(printf '%6.2f' `python -c "print round($total_not_enabled.0 / $total * 100, 2)"`)
 
 total_enabled_old=$(cat $F | grep ENABLED | grep -v PRE_ENABLED | grep 70103 | wc -l)
-percent_enabled_old=`python -c "print round($total_enabled_old.0 / $total_enabled * 100, 2)"`
+total_not_enabled_old=$(($total_old-$total_enabled_old))
+percent_enabled_old=$(printf '%6.2f' `python -c "print round($total_enabled_old.0 / $total_enabled * 100, 2)"`)
+total_percent_enabled_old=$(printf '%6.2f' `python -c "print round($total_enabled_old.0 / $total * 100, 2)"`)
+percent_not_enabled_old=$(printf '%6.2f' `python -c "print round($total_not_enabled_old.0 / $total_enabled * 100, 2)"`)
+total_percent_not_enabled_old=$(printf '%6.2f' `python -c "print round($total_not_enabled_old.0 / $total * 100, 2)"`)
 
 total_enabled_new=$(cat $F | grep ENABLED | grep -v PRE_ENABLED | grep 70206 | wc -l)
-percent_enabled_new=`python -c "print round($total_enabled_new.0 / $total_enabled * 100, 2)"`
+total_not_enabled_new=$(($total_new-$total_enabled_new))
+percent_enabled_new=$(printf '%6.2f' `python -c "print round($total_enabled_new.0 / $total_enabled * 100, 2)"`)
+total_percent_enabled_new=$(printf '%6.2f' `python -c "print round($total_enabled_new.0 / $total * 100, 2)"`)
+percent_not_enabled_new=$(printf '%6.2f' `python -c "print round($total_not_enabled_new.0 / $total_enabled * 100, 2)"`)
+total_percent_not_enabled_new=$(printf '%6.2f' `python -c "print round($total_not_enabled_new.0 / $total * 100, 2)"`)
+
+percent_not_enabled_old_of_total_old=$(printf '%6.2f' `python -c "print round($total_not_enabled_old.0 / $total_old * 100, 2)"`)
+percent_not_enabled_new_of_total_new=$(printf '%6.2f' `python -c "print round($total_not_enabled_new.0 / $total_new * 100, 2)"`)
+percent_enabled_old_of_total_old=$(printf '%6.2f' `python -c "print round($total_enabled_old.0 / $total_old * 100, 2)"`)
+percent_enabled_new_of_total_new=$(printf '%6.2f' `python -c "print round($total_enabled_new.0 / $total_new * 100, 2)"`)
 
 echo "Masternodes...."
-echo " [old]   70103: $total_old or $percent_old%"
-echo " [new]   70206: $total_new or $percent_new%"
-echo "         Total: $total"
+echo -e " [old]    70103: $(printf '%4s' $total_old) - $percent_old%"
+echo -e " [new]    70206: $(printf '%4s' $total_new) - $percent_new%"
+echo "          Total: $(printf '%4s' $total)"
 echo
-echo " [old] ENABLED: $total_enabled_old or $percent_enabled_old%"
-echo " [new] ENABLED: $total_enabled_new or $percent_enabled_new%"
-echo "         Total: $total_enabled or $percent_enabled%"
+echo -e " [old] !ENABLED: $(printf '%4s' $total_not_enabled_old) - $percent_not_enabled_old% (of !enabled) . $percent_not_enabled_old_of_total_old% (of total old) . $total_percent_not_enabled_old% (of total)"
+echo -e " [new] !ENABLED: $(printf '%4s' $total_not_enabled_new) - $percent_not_enabled_new% (of !enabled) . $percent_not_enabled_new_of_total_new% (of total new) . $total_percent_not_enabled_new% (of total)"
+echo -e "          Total: $(printf '%4s' $total_not_enabled) - $percent_not_enabled%"
+echo
+echo -e " [old]  ENABLED: $(printf '%4s' $total_enabled_old) -  $percent_enabled_old% (of enabled) . $percent_enabled_old_of_total_old% (of total old) . $total_percent_enabled_old% (of total)"
+echo -e " [new]  ENABLED: $(printf '%4s' $total_enabled_new) -  $percent_enabled_new% (of enabled) . $percent_enabled_new_of_total_new% (of total new) . $total_percent_enabled_new% (of total)"
+echo -e "          Total: $(printf '%4s' $total_enabled) -  $percent_enabled%"
 rm $F
 
