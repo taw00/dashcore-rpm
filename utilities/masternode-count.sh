769269928294014054cd37c0edbb75657213a416
Todd Warner <t0dd+gh@protonmail.com>
2017-02-13 04:26:48 -0500

    Improved masternode count with percentages.

--- utilities/masternode-count.sh
+++ utilities/masternode-count.sh
@@ -10,11 +10,30 @@ LF="sudo -u dashcore dash-cli -conf=/etc/dashcore/dash.conf masternode list full
 
 F=/tmp/tmp-count.txt
 $LF > $F
-echo "...Masternodes: $(cat $F | wc -l )"
-echo " [old]   70103: $(cat $F | grep 70103 | wc -l)"
-echo " [new]   70206: $(cat $F | grep 70206 | wc -l)"
-echo " [old] ENABLED: $(cat $F | grep ENABLED | grep -v PRE_ENABLED | grep 70103 | wc -l)"
-echo " [new] ENABLED: $(cat $F | grep ENABLED | grep -v PRE_ENABLED | grep 70206 | wc -l)"
-echo " Total ENABLED: $(cat $F | grep ENABLED | grep -v PRE_ENABLED | wc -l)"
+
+total=$(cat $F | wc -l )
+total_old=$(cat $F | grep 70103 | wc -l)
+percent_old=`python -c "print round($total_old.0 / $total * 100, 2)"`
+
+total_new=$(cat $F | grep 70206 | wc -l)
+percent_new=`python -c "print round($total_new.0 / $total * 100, 2)"`
+
+total_enabled=$(cat $F | grep ENABLED | grep -v PRE_ENABLED | wc -l)
+percent_enabled=`python -c "print round($total_enabled.0 / $total * 100, 2)"`
+
+total_enabled_old=$(cat $F | grep ENABLED | grep -v PRE_ENABLED | grep 70103 | wc -l)
+percent_enabled_old=`python -c "print round($total_enabled_old.0 / $total_enabled * 100, 2)"`
+
+total_enabled_new=$(cat $F | grep ENABLED | grep -v PRE_ENABLED | grep 70206 | wc -l)
+percent_enabled_new=`python -c "print round($total_enabled_new.0 / $total_enabled * 100, 2)"`
+
+echo "Masternodes...."
+echo " [old]   70103: $total_old or $percent_old%"
+echo " [new]   70206: $total_new or $percent_new%"
+echo "         Total: $total"
+echo
+echo " [old] ENABLED: $total_enabled_old or $percent_enabled_old%"
+echo " [new] ENABLED: $total_enabled_new or $percent_enabled_new%"
+echo "         Total: $total_enabled or $percent_enabled%"
 rm $F
 
