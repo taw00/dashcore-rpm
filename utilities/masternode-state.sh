7de45cddf8a103794b432a03123fb93b6ae882e4
Todd Warner <t0dd+gh@protonmail.com>
2017-02-16 08:17:05 -0500

    Added email support. Improved logic. This really should be Python.

--- utilities/masternode-state.sh
+++ utilities/masternode-state.sh
@@ -11,17 +11,37 @@
 # All gets logged to /var/log/dashcore/masternode-state.log and printed to the screen (stdout)
 # If there seems something amiss, masternode-state.log will exit with a non-zero error code.
 #
+# Optionally send email if NEW_START_REQUIRED or WATCHDOG_EXPIRED state is hit.
+#   - interally from the code
+#   - or, set up a cronjob and react to the exit code
+#   - HowTo set up your server to email? Check out https://github.com/taw00/howto
+#   
+#
 # Exit codes...
 # 0 = All seems well
 # 1 = Block heights do not match
 # 2 = Masternode not in ENABLED or PRE_ENABLED state
+# 3 = Both 1 and 2
 
 testnet=0
-config=/etc/dashcore/dash.conf
 ip=93.184.216.34 # Your masternode IP address
+
+email_me=0
+email_from="burner-address@yahoo.com"
+email_to="personal-address@protonmail.com"
+subject_qualifier="[masternode alias]"
+body_text=""
+
+# Output settings
+# noise - 0=silence (except errors), 1=silent except timestamp of run, 2=loud
+# logfile - If no logfile, only goes to stdout
+noise=2
 logfile=
-#logfile="/var/log/dashcore/masternode-state.log" # If no logfile, only goes to stdout
-noise=2 # 0=silence (except errors), 1=silent except timestamp of run, 2=loud
+#logfile="/var/log/dashcore/masternode-state.log"
+
+config=/etc/dashcore/dash.conf
+
+# ---- don't edit anything after this ----
 
 #username=dashcore
 true_height=
@@ -29,6 +49,7 @@ my_height=
 mn_enablement="ENABLED"
 _network_string='[mainnet]'
 _height_url="https://explorer.dash.org/chain/Dash/q/getblockcount"
+exit_code=0
 
 if [[ testnet -eq 1 ]] ; then
   _network_string='[testnet]'
@@ -37,14 +58,14 @@ fi
 
 _d=$(date --utc +"%b %d %T UTC $_network_string")
 
-if [[ noise -gt 0 ]] ; then
+if [[ $noise -gt 0 ]] ; then
   m="$_d ---"
   echo $m
   if [[ $logfile ]] ; then echo $m >> $logfile ; fi
 fi
 
 true_height=$(curl --silent -o - $_height_url)
-while [[ true_height -lt 1 ]] ; do
+while [[ $true_height -lt 1 ]] ; do
   m="$_d --- No results from $_height_url Trying again."
   echo $m
   if [[ $logfile ]] ; then echo $m >> $logfile ; fi
@@ -53,7 +74,7 @@ while [[ true_height -lt 1 ]] ; do
 done
 
 my_height=$(dash-cli -conf=$config getblockcount)
-while [[ my_height -lt 1 ]] ; do
+while [[ $my_height -lt 1 ]] ; do
   m="$_d --- No results from 'dash-cli getblockcount' Trying again."
   echo $m
   if [[ $logfile ]] ; then echo $m >> $logfile ; fi
@@ -64,12 +85,7 @@ done
 msg1="$_d Current block height is $true_height"
 if [[ $true_height -ne $my_height ]] ; then
   msg1="$_d WARNING! Height mismatch! Network height versus this node: $true_height : $my_height"
-  echo $msg1
-  if [[ $logfile ]] ; then echo $msg1 >> $logfile ; fi
-  exit 1
-elif [[ noise -gt 1 ]] ; then
-  echo $msg1
-  if [[ $logfile ]] ; then echo $msg1 >> $logfile ; fi
+  exit_code=$(($exit_code+1))
 fi
 
 # Get masternode status string
@@ -81,14 +97,34 @@ mn_enablement=$(echo -e "${mn_enablement}" | sed -e 's/^[[:space:]]*//' -e 's/[[
 # Snag the actual enablement value
 mn_enablement=$(echo -e "${mn_enablement}" | cut -d ' ' -f2)
 msg2="$_d Masternode enablement status: $mn_enablement"
+
 if [[ $mn_enablement -ne "ENABLED" && $mn_enablement -ne "PRE_ENABLED" ]] ; then
   msg2="$_d WARNING! Masternode state is not reporting as 'ENABLED' or 'PRE_ENABLED'. Reporting as: $mn_enablement"
-  echo $msg2
-  if [[ $logfile ]] ; then echo $msg2 >> $logfile ; fi
-  exit 2
-elif [[ noise -gt 1 ]] ; then
+  exit_code=$(($exit_code+2))
+fi
+
+if [[ $exit_code -gt 0 || $noise -gt 1 ]] ; then
+  echo $msg1
+  if [[ $logfile ]] ; then echo $msg1 >> $logfile ; fi
   echo $msg2
   if [[ $logfile ]] ; then echo $msg2 >> $logfile ; fi
 fi
 
-exit 0
+if [[ $email_me -ne 0 ]] ; then
+  body_text="$msg1\n$msg2\n"
+  case $mn_enablement in
+    WATCHDOG_EXPIRED|NEW_START_REQUIRED )
+      echo -e $body_text | mail -s "$subject_qualifier $mn_enablement - $_d" -r $email_from $email_to
+      m="$_d Alert email sent to $email_to from $email_from"
+      echo $m
+      if [[ $logfile ]] ; then echo $m >> $logfile ; fi
+    ;;
+  esac
+  # Other way of doing the same thing...
+  #if [[ "$mn_enablement" =~ ^(WATCHDOG_EXPIRED|NEW_START_REQUIRED)$ ]] ; then
+  #  cat $body_text | mail -s "$subject_qualifier $mn_enablement - $_d" -r $email_from $email_to
+  #fi
+fi
+
+
+exit $exit_code
