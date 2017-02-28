0002ba6c905eb3bc9f3bbcb2e7aadbc2ce67db4a
Todd Warner <t0dd@protonmail.com>
2017-02-28 18:22:47 -0500

    fix bug in percentage calculations.

--- utilities/masternode-count.sh
+++ utilities/masternode-count.sh
@@ -3,7 +3,7 @@
 # run this with 'watch -n15 . ./masternode-count.sh'
 
 # If systemd-managed...
-LF="sudo -u dashcore dash-cli -conf=/etc/dashcore/dash.conf masternode list full"
+LF="sudo -u dashcore dash-cli -conf=/etc/dashcore/dash.conf -datadir=/var/lib/dashcore masternode list full"
 
 # If "normal user" deployed..
 #LF="dash-cli masternode list full"
@@ -28,14 +28,14 @@ total_enabled_old=$(cat $F | grep ENABLED | grep -v PRE_ENABLED | grep " 70103 "
 total_not_enabled_old=$(($total_old-$total_enabled_old))
 percent_enabled_old=$(printf '%6.2f' `python -c "print round($total_enabled_old.0 / $total_enabled * 100, 2)"`)
 total_percent_enabled_old=$(printf '%6.2f' `python -c "print round($total_enabled_old.0 / $total * 100, 2)"`)
-percent_not_enabled_old=$(printf '%6.2f' `python -c "print round($total_not_enabled_old.0 / $total_enabled * 100, 2)"`)
+percent_not_enabled_old=$(printf '%6.2f' `python -c "print round($total_not_enabled_old.0 / $total_not_enabled * 100, 2)"`)
 total_percent_not_enabled_old=$(printf '%6.2f' `python -c "print round($total_not_enabled_old.0 / $total * 100, 2)"`)
 
 total_enabled_new=$(cat $F | grep ENABLED | grep -v PRE_ENABLED | grep " 70206 " | wc -l)
 total_not_enabled_new=$(($total_new-$total_enabled_new))
 percent_enabled_new=$(printf '%6.2f' `python -c "print round($total_enabled_new.0 / $total_enabled * 100, 2)"`)
 total_percent_enabled_new=$(printf '%6.2f' `python -c "print round($total_enabled_new.0 / $total * 100, 2)"`)
-percent_not_enabled_new=$(printf '%6.2f' `python -c "print round($total_not_enabled_new.0 / $total_enabled * 100, 2)"`)
+percent_not_enabled_new=$(printf '%6.2f' `python -c "print round($total_not_enabled_new.0 / $total_not_enabled * 100, 2)"`)
 total_percent_not_enabled_new=$(printf '%6.2f' `python -c "print round($total_not_enabled_new.0 / $total * 100, 2)"`)
 
 percent_not_enabled_old_of_total_old=$(printf '%6.2f' `python -c "print round($total_not_enabled_old.0 / $total_old * 100, 2)"`)
@@ -56,3 +56,4 @@ echo -e " [old]  ENABLED: $(printf '%4s' $total_enabled_old) - $percent_enabled_
 echo -e " [new]  ENABLED: $(printf '%4s' $total_enabled_new) - $percent_enabled_new%  (of enabled) . $percent_enabled_new_of_total_new% (of total new) . $total_percent_enabled_new% (of total)"
 echo -e "          Total: $(printf '%4s' $total_enabled) - $percent_enabled%"
 rm $F
+
